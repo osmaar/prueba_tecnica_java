beaa0b0ddde4c8bbad809e8e9f30a6550e600469
Osmar López Islas <osmichan07@gmail.com>
2025-09-10 17:38:29 -0600

    Inicializando el proyecto spring

---
+++ src/main/java/com/osmi/dev/duplicatefinder/DuplicateContactFinderApplication.java
@@ -0,0 +1,13 @@
+package com.osmi.dev.duplicatefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuplicateContactFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuplicateContactFinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/osmi/dev/duplicatefinder/DuplicateContactFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osmi.dev.duplicatefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuplicateContactFinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
