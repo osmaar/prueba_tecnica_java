79ffc9d77c90cdb7afbdb8b12deb0eff33f01d15
Osmar López Islas <osmichan07@gmail.com>
2025-09-11 16:36:54 -0600

    Agregar clase Contact con atributos y métodos de acceso

---
+++ src/main/java/com/osmi/dev/duplicatefinder/model/Contact.java
@@ -0,0 +1,65 @@
+package com.osmi.dev.duplicatefinder.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class Contact {
+
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String codigoPostal;
+    private String direccion;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+}
